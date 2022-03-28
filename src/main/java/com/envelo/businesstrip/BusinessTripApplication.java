e06235a49e99bc5ecf22c3257976c1a04e5b1b36
adamskas <skaskiewiczadam@gmail.com>
2022-03-28 12:26:20 +0200

    Initial commit
    Initial commit with basic depedancies. Replaced initial template with spring boot project

---
+++ src/main/java/com/envelo/businesstrip/BusinessTripApplication.java
@@ -0,0 +1,13 @@
+package com.envelo.businesstrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/envelo/businesstrip/BusinessTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.envelo.businesstrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
