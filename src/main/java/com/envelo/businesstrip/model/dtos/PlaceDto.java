2d8b0e70cd2e14c9a0b0c871d49c59da90143204
adamskas <skaskiewiczadam@gmail.com>
2022-04-05 10:31:23 +0200

    fix: fixed existing models
    fixed existing models of CountryDto, Country, PlaceDto, Place
    fixed README with minor changes

--- src/main/java/com/envelo/businesstrip/model/dtos/PlaceDto.java
+++ src/main/java/com/envelo/businesstrip/model/dtos/PlaceDto.java
@@ -1,10 +1,12 @@
-package com.envelo.businesstrip.model.dto;
+package com.envelo.businesstrip.model.dtos;
 
+import com.envelo.businesstrip.model.dtos.CountryDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -18,11 +20,9 @@ public class PlaceDto {
     @Size(min = 2, max = 50)
     private String city;
 
-    @NotBlank
     @Size(min = 2, max = 50)
     private String street;
 
-    @NotBlank
     @Size(min = 2, max = 50)
     private String postCode;
 
