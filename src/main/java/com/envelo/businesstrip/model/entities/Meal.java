d58f274d3c4c40a7abd218c4711ef063c947095d
Artur Malinowski <artur.malinowski@envelo.pl>
2022-04-06 13:29:10 +0000

    Update Meal.java

--- src/main/java/com/envelo/businesstrip/model/entities/Meal.java
+++ src/main/java/com/envelo/businesstrip/model/entities/Meal.java
@@ -9,21 +9,19 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Embeddable
+@Table(name = "meals")
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
-@Table(name = "meals")
 public class Meal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable=false)
     private int numberOfBreakfasts;
 
-    @Column(nullable=false)
     private int numberOfLunches;
 
-    @Column(nullable=false)
     private int numberOfDinners;
 }
