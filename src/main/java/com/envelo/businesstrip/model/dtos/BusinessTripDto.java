c0ceb697e1ba7a59155b9590c3baae5f76440d65
Artur Malinowski <artur.malinowski@envelo.pl>
2022-04-07 13:33:07 +0000

    Ae 275/feat/am/business trip dto

---
+++ src/main/java/com/envelo/businesstrip/model/dtos/BusinessTripDto.java
@@ -0,0 +1,42 @@
+package com.envelo.businesstrip.model.dtos;
+
+import com.envelo.businesstrip.model.enums.BusinessTripStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class BusinessTripDto {
+
+    private Long id;
+
+    private String code;
+    @NotBlank(message = "Departament must not be null.")
+    @Size(min = 2, max = 50, message = "Length of departament must be between 2 to 50 characters.")
+    private String department;
+
+    @NotBlank(message = "Position must not be null.")
+    @Size(min = 2, max = 50, message = "Length of position must be between 2 to 50 characters.")
+    private String position;
+
+    @NotNull(message = "Application form must not be null.")
+    private ApplicationForm applicationForm;
+
+    private ExpenseReport expenseReport;
+
+    @NotNull(message = "Status must not be null.")
+    private BusinessTripStatus businessTripStatus;
+
+    public BusinessTripDto(String code, String department, String position, ApplicationForm applicationForm,  BusinessTripStatus businessTripStatus) {
+        this.code = code;
+        this.department = department;
+        this.position = position;
+        this.applicationForm = applicationForm;
+        this.businessTripStatus = businessTripStatus;
+    }
+}
