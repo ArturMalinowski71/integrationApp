3fe1e4e956541317bb141e003bc2959d8849b78b
przemyslaw.nosek <przemyslaw.nosek@ppuc.pl>
2022-04-08 14:12:26 +0200

    feat: added 2 repos and wrote unit test for custom method
    added 2 repos and wrote 1 sql and unit tested it

--- src/test/java/com/envelo/businesstrip/repositories/CommentRepositoryTest.java
+++ src/test/java/com/envelo/businesstrip/repositories/CommentRepositoryTest.java
@@ -5,26 +5,20 @@ import com.envelo.businesstrip.model.entities.ExpenseReport;
 import com.envelo.businesstrip.model.entities.User;
 import com.envelo.businesstrip.model.enums.Department;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
-@ExtendWith(SpringExtension.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 @DataJpaTest
 @Transactional
-@Rollback
 public class CommentRepositoryTest {
 
     @Autowired
@@ -34,42 +28,55 @@ public class CommentRepositoryTest {
     private TestEntityManager entityManager;
 
     @Test
-    void should() {
-        User user = new User(
-                "przemek",
-                "email",
-                "Przemek",
-                "Przemek",
-                "Przemek",
-                "Przemek",
-                "fsaf",
-                new HashSet<>(),
-                new HashSet<>());
+    void findAllByExpenseReportId_shouldReturnListOfComments_whenCommentsAreConnectedToExpenseReport() {
+        //given
+        User user = User.builder()
+                .id("id")
+                .email("email")
+                .firstName("firstName")
+                .lastName("lastName")
+                .department("department")
+                .title("title")
+                .supervisor("supervisor")
+                .roles(new HashSet<>())
+                .build();
+
 
         List<Comment> comments = List.of(
-                new Comment(user, Department.ACCOUNTANT, LocalDateTime.now(), "fajne"),
-                new Comment(user, Department.ACCOUNTANT, LocalDateTime.now(), "fajne1"),
-                new Comment(user, Department.ACCOUNTANT, LocalDateTime.now(), "fajne2")
+                new Comment(user, Department.ACCOUNTANT, LocalDateTime.now(), "conent1"),
+                new Comment(user, Department.ACCOUNTANT, LocalDateTime.now(), "conent2"),
+                new Comment(user, Department.ACCOUNTANT, LocalDateTime.now(), "conent3")
         );
 
-        ExpenseReport expenseReport = new ExpenseReport(
-                LocalDateTime.now(),
-                LocalDateTime.now(),
-                user,
-                user,
-                LocalDateTime.now(),
-                LocalDateTime.now(),
-                "fsfa",
-                new ArrayList<>(),
-                new ArrayList<>(),
-                comments);
+        ExpenseReport expenseReportWithComments = ExpenseReport
+                .builder()
+                .creationTime(LocalDateTime.now())
+                .supervisor(user)
+                .actualStartDate(LocalDateTime.now())
+                .actualEndDate(LocalDateTime.now())
+                .bankAccount("bankAccount")
+                .comments(comments)
+                .build();
 
-        Object o = entityManager.persistAndGetId(expenseReport);
 
-//        System.out.println(o.toString());
-        List<Comment> allByExpenseReportId =
-                commentRepository.findAllByExpenseReportId(Long.parseLong(o.toString()));
+        ExpenseReport expenseReportWithoutComments = ExpenseReport
+                .builder()
+                .creationTime(LocalDateTime.now())
+                .supervisor(user)
+                .actualStartDate(LocalDateTime.now())
+                .actualEndDate(LocalDateTime.now())
+                .bankAccount("bankAccount")
+                .build();
 
-        allByExpenseReportId.forEach(System.out::println);
+        Long expenseReportWithCommentsId = (Long)entityManager.persistAndGetId(expenseReportWithComments);
+        Long expenseReportWithoutCommentsId = (Long)entityManager.persistAndGetId(expenseReportWithoutComments);
+
+        //when
+        List<Comment> allByExpenseReportWithCommentsId = commentRepository.findAllByExpenseReportId(expenseReportWithCommentsId);
+        List<Comment> allByExpenseReportWithoutCommentsId = commentRepository.findAllByExpenseReportId(expenseReportWithoutCommentsId);
+
+        //then
+        assertThat(allByExpenseReportWithCommentsId).hasSize(3);
+        assertThat(allByExpenseReportWithoutCommentsId).hasSize(0);
     }
 }
